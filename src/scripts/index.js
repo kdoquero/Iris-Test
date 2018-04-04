483714b0941311987d290c0be3f952a4c312be39
kdoquero <kevindoquero@gmail.com>
2018-04-04 16:27:17 +0200

    added style for mobile

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,22 @@
 
-import '../scss/index.scss';
 import $ from "jquery";
+
+
+
+$( ".asked-question" ).tooltip({
+    position: {
+        my: "right center",
+        at: "left-10 center",
+        collision: "flip",
+        using: function( position, feedback ) {
+            $( this ).addClass( feedback.vertical )
+                .css( position );
+        }
+    },
+    classes: {
+        "ui-tooltip": "highlight"
+      }
+
+});
+import '../scss/index.scss';
+
