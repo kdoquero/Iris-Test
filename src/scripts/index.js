9d16e7bee33ed392937cc46cb4f8c704ad8054a8
kdoquero <kevindoquero@gmail.com>
2018-04-05 08:49:44 +0200

    added jquery

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,22 +1,26 @@
 
 import $ from "jquery";
-
-
-
-$( ".asked-question" ).tooltip({
-    position: {
-        my: "right center",
-        at: "left-10 center",
-        collision: "flip",
-        using: function( position, feedback ) {
-            $( this ).addClass( feedback.vertical )
-                .css( position );
-        }
-    },
-    classes: {
-        "ui-tooltip": "highlight"
-      }
-
-});
 import '../scss/index.scss';
+
+$(document).ready(function () {
+    $( ".asked-question[title]" ).tooltip({
+        position: {
+            my: "right center",
+            at: "left-40 center",
+            collision: "flip",
+            using: function( position, feedback ) {
+                $( this ).addClass( feedback.vertical )
+                    .css( position );
+            }
+        },
+        tooltipClass: "QA"
+    
+    });
+    
+});
+
+
+
+
+
 
